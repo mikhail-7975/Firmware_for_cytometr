a290d492d5a7659c923078efe3c369a6dbce179c
Михаил Калиниченко <mikhail.kalinichenko.1999@gmail.com>
2020-09-15 13:26:15 +0700

    Dma

--- TestDMA/Core/Src/main.c
+++ TestDMA/Core/Src/main.c
@@ -142,7 +142,7 @@ int main(void)
 	  		//	  dataToTransmit[i] = 123;
 	  		  //}
 	  		  isTransmited = 1;
-	  		  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&dataFromADC, TRACE_SIZE * 2);
+	  		  //HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&dataFromADC, TRACE_SIZE * 2);
 	  	  }
     /* USER CODE END WHILE */
 
@@ -169,7 +169,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -183,13 +183,13 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -286,7 +286,7 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 
 void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
 {
-	HAL_ADC_Stop_DMA(&hadc1);
+	//HAL_ADC_Stop_DMA(&hadc1);
 	if (isTransmited == 1) {
 		memcpy(&dataToTransmit, &dataFromADC, TRACE_SIZE * sizeof(uint16_t));
 		isReadyToSend = 1;
